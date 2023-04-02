7b35b48d3b119a3a0d7c8597d7148ca9ee36ebbe
CyberZ <47226769+CyberZBit@users.noreply.github.com>
2023-04-02 15:31:24 +0200

    Add files via upload

--- dev/editProfile.php
+++ dev/editProfile.php
@@ -19,30 +19,68 @@
             <a href="./editProfile.php">Edit Profile</a>
             <a href="./logout.php">Logout</a>
         </nav>
-                    <div class="form-container">
-            <h2>Account Info</h2>
-            <form>
-                <label for="username">Username:</label>
-                <input type="text" id="username" name="username" value="JohnDoe" placeholder="Enter username">
-                
-                <label for="password">Old Password:</label>
-                <input type="password" id="password" name="password" placeholder="Enter old password">
-                
-                <label for="newpassword1">New Password:</label>
-                <input type="password" id="newpassword1" name="newpassword1" placeholder="Enter new password">
-                
-                <label for="newpassword2">Confirm New Password:</label>
-                <input type="password" id="newpassword2" name="newpassword2" placeholder="Confirm new password">
-                
-                <label for="firstname">First Name:</label>
-                <input type="text" id="firstname" name="firstname" value="John" placeholder="Enter first name">
-                
-                <label for="lastname">Last Name:</label>
-                <input type="text" id="lastname" name="lastname" value="Doe" placeholder="Enter last name">
-                
-                <input type="submit" value="Save Changes">
-            </form>
-            </div>
+          
+        
+        <div class="form-container">
+  <h2>Account Info</h2>
+  
+  <form id="form-one" action="submit-one.php" method="post">
+    <div class="section-container one">
+
+      <div class="form-group one">
+        <label for="firstname">First Name</label>
+        <input type="text" id="firstname" name="firstname" placeholder="Enter your first name" value="Jon">
+      </div>
+
+      <div class="form-group one">
+        <label for="lastname">Last Name</label>
+        <input type="text" id="lastname" name="lastname" placeholder="Enter your last name" value="Doe">
+      </div>
+
+      <div class="form-group one">
+        <label for="email">Email</label>
+        <input type="text" id="email" name="email" placeholder="Enter your email" value="admin@site.net">
+      </div>
+    
+      <button type="button" onclick="showSection('two')">Reset Password</button>
+      <input type="submit" value="Save Changes">
+    </div>
+  </form>
+
+  <form id="form-two" action="submit-two.php" method="post">
+    <div class="section-container two" style="display: none">
+      <div class="form-group two">
+        <label for="oldpassword">Old Password</label>
+        <input type="password" id="oldpassword" name="oldpassword" placeholder="Enter your old password">
+      </div>
+
+      <div class="form-group two">
+        <label for="newpassword">New Password</label>
+        <input type="password" id="newpassword" name="newpassword" placeholder="Enter your new password">
+      </div>
+
+      <div class="form-group two">
+        <label for="confirmpassword">Confirm New Password</label>
+        <input type="password" id="confirmpassword" name="confirmpassword" placeholder="Confirm your new password">
+      </div>
+
+      <button type="button" onclick="showSection('one')">Cancel</button>
+      <input type="submit" value="Save Changes">
+    </div>
+  </form>
+</div>
+
+<script>
+  function showSection(section) {
+    if (section === 'one') {
+      document.querySelector('#form-one .section-container.one').style.display = 'block';
+      document.querySelector('#form-two .section-container.two').style.display = 'none';
+    } else {
+      document.querySelector('#form-one .section-container.one').style.display = 'none';
+      document.querySelector('#form-two .section-container.two').style.display = 'block';
+    }
+  }
+</script>
 
     </body>
     </html>
